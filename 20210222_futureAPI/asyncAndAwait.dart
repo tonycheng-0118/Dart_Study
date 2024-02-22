35b3bef729a4c406efee489555aa9eaf2257229d
tonycheng-0118 <chingjucheng0118@gmail.com>
2024-02-22 23:02:19 +0800

    [Dart] test auto indent shortcut
      - Shift + Alt + F - auto indent
      - add asyncAndAwait.dart

--- 20210222_futureAPI/asyncAndAwait.dart
+++ 20210222_futureAPI/asyncAndAwait.dart
@@ -3,8 +3,13 @@ import 'dart:async';
 String countUp(int count) {
   print('start count up');
   StringBuffer sb = new StringBuffer();
-  for (int i = 0; i < count; i++) {
-    sb.write("${i}");
+
+  if (count > 500) {
+    throw new Exception('Over 500 not allowed.');
+  } else {
+    for (int i = 0; i < count; i++) {
+      sb.write("${i}");
+    }
   }
   print('finish count up');
   return sb.toString();
@@ -18,14 +23,18 @@ Future<String> createFutureCounter(int count) {
 
 void countUpAsync(int count) async {
   print('Async operation start');
-  String value = await createFutureCounter(count);
-  print('Async operation succeeded: ${value}');
+  try {
+    String value = await createFutureCounter(count);
+    print('Async operation succeeded: ${value}');
+  } catch (ex) {
+    print('Async operation errored: ${ex}}');
+  }
 }
 
 void main() {
   print('start main');
   // Future<String> future = createFutureCounter(100);
-  countUpAsync(100);
+  countUpAsync(1000);
   // future.then((value) => handleCompletion(value));
 }
 
